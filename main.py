45d11a62f9d612446cb471581c9e2b5d61a21223
Ст Ст <stst@MacBook-Air-St.local>
2025-05-18 13:11:25 +0300

    user agents

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import requests
+from user_agents import parse
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -13,31 +14,35 @@ async def get_ip(request: Request):
     forwarded = request.headers.get("x-forwarded-for")
     client_ip = forwarded.split(",")[0] if forwarded else request.client.host
 
-    user_agent = request.headers.get("user-agent", "Unknown")
-
-    # Отримуємо дані з ipwho.is
     response = requests.get(f"https://ipwho.is/{client_ip}")
     try:
         data = response.json()
     except ValueError:
         data = {}
 
-    # Прапор
+    # User-Agent
+    user_agent_string = request.headers.get("user-agent", "")
+    user_agent = parse(user_agent_string)
+
     flag_url = f"https://flagcdn.com/256x192/{data.get('country_code','').lower()}.png" if data.get('country_code') else None
 
-    # Підготовка даних для шаблону
     return templates.TemplateResponse("index.html", {
         "request": request,
         "ip": client_ip,
-        "ip_type": data.get("type", "Unknown"),
+        "type": data.get("type", "Unknown"),
+        "isp": data.get("connection", {}).get("isp", "Unknown"),
+        "asn": data.get("connection", {}).get("asn", "Unknown"),
+        "hostname": data.get("connection", {}).get("domain", "Unknown"),
+        "is_proxy": data.get("security", {}).get("proxy", False),
+        "is_vpn": data.get("security", {}).get("vpn", False),
+        "is_tor": data.get("security", {}).get("tor", False),
+        "user_agent": user_agent_string,
+        "browser": user_agent.browser.family,
+        "os": user_agent.os.family,
         "city": data.get("city", "Unknown"),
         "region": data.get("region", "Unknown"),
         "country": data.get("country", "Unknown"),
         "latitude": data.get("latitude"),
         "longitude": data.get("longitude"),
         "flag_url": flag_url,
-        "isp": data.get("connection", {}).get("isp", "Unknown"),
-        "asn": data.get("connection", {}).get("asn", "Unknown"),
-        "org": data.get("connection", {}).get("org", "Unknown"),
-        "user_agent": user_agent,
     })
